f056980f701b9e3d17c73d295bfa0a86808f43f3
KANISHKA R <kanishka.r221@gmail.com>
2025-09-03 13:32:33 -0500

    Update tb_and_gate.v

--- Verilog/DAY1/LogicGate/and_gate/tb_and_gate.v
+++ Verilog/DAY1/LogicGate/and_gate/tb_and_gate.v
@@ -1,1 +1,26 @@
+module and_gate_tb;
+        reg a, b;
+        wire y;
+
+        and_gate uut (
+                .a(a),
+                .b(b),
+                .y(y)
+        );
+
+        initial begin
+                $dumpfile("and_gate.vcd");
+                $dumpvars(0, and_gate_tb);
+
+                $display("time\t a b y");
+                $monitor("%0dns\t %b %b %b",$time, a, b, y);
+
+                a = 0; b = 0;
+                #10 a = 0; b = 1;
+                #10 a = 1; b = 0;
+                #10 a = 1; b = 1;
+                #10 $finish;
+
+        end
+endmodule
 
