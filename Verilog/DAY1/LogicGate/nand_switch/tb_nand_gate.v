796f1890b13c403f501a5cf0ea952c5825ee751e
KANISHKA R <kanishka.r221@gmail.com>
2025-09-04 06:21:27 -0500

    Add files via upload

---
+++ Verilog/DAY1/LogicGate/nand_switch/tb_nand_gate.v
@@ -0,0 +1,21 @@
+module nand_gate_tb;
+	reg a, b;
+	wire y;
+
+	nand_gate uut (.a(a), .b(b), .y(y));
+	initial  begin
+		$dumpfile("dump.vcd");
+		$dumpvars(0, nand_gate_tb);
+
+		$display("Time | a b | y");
+		$monitor("%4t | %b %b | %b", $time, a, b, y);
+
+		a=0; b=0; #10;
+		a=0; b=1; #10;
+		a=1; b=0; #10;
+ 		a=1; b=1; #10;
+		$finish;
+	end
+	endmodule
+
+
