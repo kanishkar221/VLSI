4b56fb8045fc6b320e467f0e0a40f37258b85816
KANISHKA R <kanishka.r221@gmail.com>
2025-09-05 13:17:20 -0500

    Add files via upload

---
+++ Verilog/DAY2/Dataflow_Modeling/Full_Adder/full_adder.v
@@ -0,0 +1,9 @@
+module full_adder (input a, b, cin, output sum, cout);
+	wire w1, w2, w3;
+	xor (w1, a, b); 
+	xor (sum, w1, cin); 
+	and (w2, a, b);     
+	and (w3, w1, cin);  
+	or  (cout, w2, w3);
+endmodule
+ 
