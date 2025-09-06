119752e2a44686ca631dbdfb07f9c723c463ee2b
KANISHKA R <kanishka.r221@gmail.com>
2025-09-06 11:15:42 -0500

    Add files via upload

---
+++ Verilog/ASSIGNMENTS/Assignment1/Wire_and_Reg_Demonstration/wire_reg_demo.v
@@ -0,0 +1,12 @@
+module wire_reg_demo;
+  wire net1, net2;
+  
+  assign net1= 1'b1;
+  assign net2= net1;
+  
+  initial begin 
+    $display(" net1= %b , net2= %b ", net1,net2);
+  end
+endmodule
+
+
