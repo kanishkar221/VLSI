6e0e882c8bf604e39a6982622579cf878817574a
KANISHKA R <kanishka.r221@gmail.com>
2025-09-06 11:50:45 -0500

    Update sign_int.v

--- Verilog/ASSIGNMENTS/Assignment1/Signed Integer Simulation/sign_int.v
+++ Verilog/ASSIGNMENTS/Assignment1/Signed Integer Simulation/sign_int.v
@@ -2,9 +2,9 @@ module sign_int;
 	integer i;
 	initial begin
 	$monitor("time=%0t : i(binary)=%b : i(decimal)=%d",$time,i[7:0],i);
-	i=45;#10;
-	i=87;#10;
-	i=12;#10;
+	i=34;#10;
+	i=67;#10;
+	i=91;#10;
 	$finish;
 	end
 endmodule
