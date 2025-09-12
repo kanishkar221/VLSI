5886b8a45ca5b5d9637013e76f28dd6575c22d1c
KANISHKA R <kanishka.r221@gmail.com>
2025-09-12 12:13:44 +0530

    Add files via upload

---
+++ Verilog/ASSIGNMENTS/ASSIGNMENT_5/F. Case, Casex, Casez Statements/45)4:1 mux using case/mux4to1_case.v
@@ -0,0 +1,17 @@
+module mux4to1_case (
+	input  [1:0] sel,   
+	input  a, b, c, d,  
+	output reg y        
+);
+	always @(*) begin
+		case (sel)
+			2'b00: y = a;
+			2'b01: y = b;
+     			2'b10: y = c;
+          		2'b11: y = d;
+           		default: y = 1'bx; 
+      		endcase
+   	end
+endmodule
+
+
---
+++ Verilog/ASSIGNMENTS/ASSIGNMENT_5/F. Case, Casex, Casez Statements/45)4:1 mux using case/tb_mux4to1_case.v
@@ -0,0 +1,26 @@
+module tb_mux4to1_case;
+	reg a, b, c, d;
+	reg [1:0] sel;
+	wire y;
+
+	mux4to1_case uut (
+      		.sel(sel),
+		.a(a),
+      		.b(b),
+      		.c(c),
+     		.d(d),
+      		.y(y)
+  	);
+	
+	initial begin
+		$monitor("Time=%0t | sel=%b | a=%b b=%b c=%b d=%b | y=%b", $time, sel, a, b, c, d, y);
+		a = 1; b = 0; c = 1; d = 0;
+		sel = 2'b00; #10;
+		sel = 2'b01; #10;
+      		sel = 2'b10; #10;
+      		sel = 2'b11; #10;
+        
+		$finish;
+	end
+endmodule
+
