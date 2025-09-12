96a21bd11387c4642226be31d8fabbc656a6a4c5
KANISHKA R <kanishka.r221@gmail.com>
2025-09-12 09:36:36 +0530

    Add files via upload

---
+++ Verilog/ASSIGNMENTS/ASSIGNMENT_5/C. Race Conditions & Scheduling/20)race_nonblock/race_condition.v
@@ -0,0 +1,35 @@
+module race_condition;
+	reg a, clk;
+	always @(posedge clk)
+		a <= 0;
+
+	always @(posedge clk)
+	        a <= 1;
+	initial begin
+		clk = 0;
+		forever #5 clk = ~clk;
+	end
+	initial begin
+		$monitor("Time=%t | clk=%b | a=%b", $time, clk, a);
+		#100 $finish;
+	end
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
