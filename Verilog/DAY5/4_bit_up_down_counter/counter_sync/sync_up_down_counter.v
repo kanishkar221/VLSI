dfd88284b5d76975b69fac5344c63fe35e04a9a7
KANISHKA R <kanishka.r221@gmail.com>
2025-09-16 16:04:43 +0530

    Add files via upload

---
+++ Verilog/DAY5/4_bit_up_down_counter/counter_sync/sync_up_down_counter.v
@@ -0,0 +1,18 @@
+module sync_up_down_counter (
+    input clk,          
+    input reset,        
+    input up_down,      
+    output reg [3:0] count
+);
+
+always @(posedge clk) begin
+    if (reset)
+        count <= 4'b0000;       
+    else if (up_down)
+        count <= count + 1;     
+    else
+        count <= count - 1;     
+end
+
+endmodule
+
