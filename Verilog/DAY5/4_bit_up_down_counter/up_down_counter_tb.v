b3288b15e265168262bae85b27b795ad1f0871a5
KANISHKA R <kanishka.r221@gmail.com>
2025-09-16 14:07:29 +0530

    Add files via upload

---
+++ Verilog/DAY5/4_bit_up_down_counter/up_down_counter_tb.v
@@ -0,0 +1,34 @@
+module up_down_counter_tb;
+
+	reg clk, reset, up_down;
+	wire [3:0] count;
+
+	up_down_counter uut (
+		.clk(clk),
+     		.reset(reset),
+     		.up_down(up_down),
+      		.count(count)
+  	);
+
+	initial begin
+		clk = 0;
+		forever #5 clk = ~clk;
+  	end
+
+	initial begin
+		$dumpfile("up_down_counter.vcd");
+    		$dumpvars(0, up_down_counter_tb);
+
+    		$monitor("Time=%0t | reset=%b | up_down=%b | count=%b", $time, reset, up_down, count);
+
+                reset = 1; up_down = 1; #10;
+		reset = 0;
+
+		up_down = 1; #80;
+		up_down = 0; #80;
+
+		$finish;
+	end
+
+endmodule
+
