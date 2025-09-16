d6a8f9f0d79129c8356123af4fe0c68eafef69f6
KANISHKA R <kanishka.r221@gmail.com>
2025-09-16 11:26:47 +0530

    Add files via upload

---
+++ Verilog/DAY4/7. SR_LATCH/01. SR_Latch_sync/SRLatch_sync.v
@@ -0,0 +1,22 @@
+module SRLatch_sync (
+    input wire clk,
+    input wire s, r, en,
+    input wire reset,   
+    output reg q
+);
+
+always @(posedge clk) begin
+    if (reset)
+        q <= 1'b0;      
+    else if (en) begin
+        case ({s,r})
+            2'b00: q <= q;      
+            2'b10: q <= 1'b1;   
+            2'b01: q <= 1'b0;   
+            2'b11: q <= 1'b0;   
+        endcase
+    end
+end
+
+endmodule
+
