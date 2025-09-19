288215358ae568901659c0dcc2b65c6c611a8101
KANISHKA R <kanishka.r221@gmail.com>
2025-09-19 22:28:10 +0530

    Add files via upload

---
+++ Verilog/ASSIGNMENTS/Assignment_7/C) SYSTEM TASKS & SIMULATION CONTROL/43. Random_generate/tb_random.v
@@ -0,0 +1,20 @@
+module tb_random;
+	reg  [3:0] a, b;     
+	wire [4:0] sum;
+
+	assign sum = a + b;
+	integer i;
+
+	initial begin
+	$display("Time |   A   |   B   |  SUM");
+	$monitor("%4t | %4d | %4d | %4d", $time, a, b, sum);
+
+	for (i = 0; i < 10; i = i + 1) begin
+		a = $random % 16; 
+		b = $random % 16; 
+		#5; 
+	end
+	$finish;
+	end
+endmodule
+
