2f4bff928d11bf3b027f81d818bd16f24e7ae11f
KANISHKA R <kanishka.r221@gmail.com>
2025-09-24 22:08:43 +0530

    Add files via upload

---
+++ Verilog/DAY7/1011_Sequence_Detector/Mealy/Overlap/overlap_tb.v
@@ -0,0 +1,36 @@
+module overlap_tb;
+	reg clk,rst,x;
+	wire y;
+  
+	overlap uut(.clk(clk), .rst(rst), .x(x), .y(y));
+
+	initial clk = 0;   
+	always #5 clk = ~clk;
+    
+	initial begin
+		$dumpfile("overlap.vcd");
+		$dumpvars(0, overlap_tb);
+
+		$monitor ("Time = %0t | clk = %b | rst = %b | state = %b | next_state = %b | x = %b | y = %b " , $time,clk,rst,uut.state,uut.next_state,x,y);
+		
+		x = 0;
+		#5 rst = 1;
+		#5 rst = 0;
+    
+		#10 x = 1;
+		#10 x = 0;
+		#10 x = 1;
+		#10 x = 1;
+		#10 x = 1;
+		#10 x = 0;
+		#10 x = 1;
+		#10 x = 1;
+		#10 x = 1;
+		#10 x = 0;
+		#10 x = 1;
+		#10 x = 1;
+		#10 $finish;
+	end
+endmodule
+
+
